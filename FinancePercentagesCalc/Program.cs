e4e89b6fa10f37adbd2b027d562c39bbf278815e
danmcadams <danmcadams@gmail.com>
2025-07-21 23:18:39 -0500

    Update PromptForInput, now returns proper type and default value. Need to work on compoundFrequency and age of account.

--- FinancePercentagesCalc/Program.cs
+++ FinancePercentagesCalc/Program.cs
@@ -1,102 +1,126 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
 Console.WriteLine("------------ Savings Calculator ------------");
 Console.WriteLine("A console application that calculates and displays the growth of a savings account with monthly contributions and daily-compounded interest");
 Console.WriteLine();
 
-var inputStartingBalance = Calculator.PromptForInput("Starting Balance (in us dollars): $");
-var inputMonths = Calculator.PromptForInput("Account age (in months): ");
-var inputInterestRate = Calculator.PromptForInput("Annual rate of return (percentage): ");
-var inputMonthlyContribution = Calculator.PromptForInput("Monthly Contribution: $");
-var inputCompoundFrequency = Calculator.PromptForInput("Compound Frequency (1=Daily, 2=Monthly, 3=Yearly): ");
+var startAmount = ConsoleUI.PromptForInput<double>("Starting Balance (in us dollars): $", 0);
+var interestRate = ConsoleUI.PromptForInput<double>("Annual rate of return (percentage): ", 0.0);
+var monthlyContribution = ConsoleUI.PromptForInput<double>("Monthly Contribution: $", 0);
+var inputCompoundFrequency = ConsoleUI.PromptForInput<int>("Compound Frequency [1=Daily (default), 2=Monthly, 3=Yearly]: ", 1);
 
-double.TryParse(inputStartingBalance, out var startAmount);
-int.TryParse(inputMonths, out var months);
-
-double.TryParse(inputInterestRate, out var interestRate);
-interestRate /= 100;
-
-double.TryParse(inputMonthlyContribution, out var monthlyContribution);
-var currentBalance = startAmount;
-
-var frequency = "1";
-switch (inputCompoundFrequency)
+var compoundFrequency = (CompoundFrequency) inputCompoundFrequency switch
 {
-    case "2":
-        frequency = "1";
-        break;
-}
-var apy = Math.Pow(1 + interestRate / 365, 365) - 1;
-Console.WriteLine(apy);
+    CompoundFrequency.Monthly => ConsoleUI.PromptForInput<int>("Account age in months (default 12): ", 12),
+    CompoundFrequency.Yearly => ConsoleUI.PromptForInput<int>("Account age in years (default 1): ", 1),
+    _ => 365
+};
+
+Console.WriteLine(compoundFrequency);
 Environment.Exit(0);
+// interestRate /= 100;
+// var currentBalance = startAmount;
+// Console.WriteLine(compoundFrequency);
+// var apy = Math.Pow(1 + interestRate / 365, 365) - 1;
+// // Console.WriteLine(apy);
+// // Environment.Exit(0);
+//
+// var periods = new List<Period>();
+// for (var month = 1; month <= months; month++)
+// {
+//     var monthStart = currentBalance;
+//     // AI generated ------
+//     int daysInMonth = DateTime.DaysInMonth(DateTime.Now.Year, ((DateTime.Now.Month - 1 + month - 1) % 12) + 1);
+//     // ---------
+//     // var apy = Math.Pow(1 + interestRate / daysInMonth, daysInMonth) - 1;
+//     var holdValue = currentBalance + (currentBalance * apy); // just for debugging
+//     
+//     for (var d = 0; d < daysInMonth; d++)
+//     {
+//         var dailyInterestInDollars = (currentBalance * interestRate) / 365;
+//         currentBalance += dailyInterestInDollars;
+//     }
+//     var interestForMonth = currentBalance - monthStart;
+//     currentBalance += monthlyContribution;
+//     
+//     periods.Add(
+//         new Period
+//         {
+//             Month = month,
+//             StartingBalance = monthStart,
+//             EndingBalance = currentBalance,
+//             InterestEarned = interestForMonth,
+//         });
+// }
+// double totalInterestEarned = currentBalance - ((monthlyContribution * months) + startAmount);
+//
+// Console.WriteLine();
+// Console.WriteLine($"Ending Balance: {(currentBalance):C2}");
+// Console.WriteLine($"Interest Earned: {(totalInterestEarned):C2}");
+// Console.WriteLine();
+// Console.WriteLine("Would you like to see a complete breakdown? (y/n) ");
+//
+// if (Console.ReadKey(intercept: true).KeyChar == 'y' || Console.ReadKey(intercept: true).KeyChar == 'Y')
+// {
+//     ConsoleUI.PrintBreakdownToConsole(periods);
+// }
 
-var monthlyBreakdown = new List<Period>();
-for (var month = 1; month <= months; month++)
+public enum CompoundFrequency
 {
-    var monthStart = currentBalance;
-    // AI generated ------
-    int daysInMonth = DateTime.DaysInMonth(DateTime.Now.Year, ((DateTime.Now.Month - 1 + month - 1) % 12) + 1);
-    // ---------
-    // var apy = Math.Pow(1 + interestRate / daysInMonth, daysInMonth) - 1;
-    var holdValue = currentBalance + (currentBalance * apy); // just for debugging
-    
-    for (var d = 0; d < daysInMonth; d++)
+    Daily = 1,
+    Monthly = 2,
+    Yearly = 3,
+}
+
+public readonly struct Period
+{
+    public int Month { get; init; }
+    public double StartingBalance { get; init; }
+    public double EndingBalance { get; init; }
+    public double InterestEarned { get; init; }
+
+    public string GetMonthText()
     {
-        var dailyInterestInDollars = (currentBalance * interestRate) / 365;
-        currentBalance += dailyInterestInDollars;
-    }
-    var interestForMonth = currentBalance - monthStart;
-    currentBalance += monthlyContribution;
-    
-    monthlyBreakdown.Add(
-        new Period
-        {
-            Month = month,
-            StartingBalance = monthStart,
-            EndingBalance = currentBalance,
-            InterestEarned = interestForMonth,
-        });
-}
-double totalInterestEarned = currentBalance - ((monthlyContribution * months) + startAmount);
-
-Console.WriteLine();
-Console.WriteLine($"Ending Balance: {(currentBalance):C2}");
-Console.WriteLine($"Interest Earned: {(totalInterestEarned):C2}");
-Console.WriteLine();
-Console.WriteLine("Would you like to see a complete breakdown? (y/n) ");
-
-if (Console.ReadKey(intercept: true).KeyChar == 'y' || Console.ReadKey(intercept: true).KeyChar == 'Y')
-{
-    Console.WriteLine();
-    Console.WriteLine("Month      Starting Balance      Interest Earned      Ending Balance");
-    Console.WriteLine("====================================================================");
-
-    foreach (var p in monthlyBreakdown)
-    {
-        var period = p.Month.ToString().PadRight(11);
-        var start = p.StartingBalance.ToString("C2").PadLeft(16);
-        // var start = p.StartingBalance.ToString("C2").PadRight(22);
-        var interestEarned = p.InterestEarned.ToString("C2").PadLeft(21);
-        var end = p.EndingBalance.ToString("C2").PadLeft(18);
-
-        Console.WriteLine($"{period}{start}{interestEarned}{end}");
+        return Month.ToString();
     }
 }
 
-internal struct Period
+public class ConsoleUI
 {
-    public int Month;
-    public double StartingBalance;
-    public double EndingBalance;
-    public double InterestEarned;
-}
-
-internal class Calculator
-{
-    public static string PromptForInput(string prompt)
+    public static T PromptForInput<T>(string prompt, T defaultValue)
     {
         Console.Write(prompt);
-        string? input = Console.ReadLine();
-        return string.IsNullOrEmpty(input) ? "0" : input;
+        var input = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(input))
+        {
+            return defaultValue;
+        }
+
+        try
+        {
+            return (T)Convert.ChangeType(input, typeof(T));
+        }
+        catch
+        {
+            return defaultValue;
+        }
+    }
+
+    public static void PrintBreakdownToConsole(List<Period> periods)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Month      Starting Balance      Interest Earned      Ending Balance");
+        Console.WriteLine("====================================================================");
+
+        foreach (var p in periods)
+        {
+            var period = p.Month.ToString().PadRight(11);
+            var start = p.StartingBalance.ToString("C2").PadLeft(16);
+            // var start = p.StartingBalance.ToString("C2").PadRight(22);
+            var interestEarned = p.InterestEarned.ToString("C2").PadLeft(21);
+            var end = p.EndingBalance.ToString("C2").PadLeft(18);
+
+            Console.WriteLine($"{period}{start}{interestEarned}{end}");
+        }
     }
 }
