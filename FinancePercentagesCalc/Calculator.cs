1d129165ebf233e63861ea7c4bf8d204f514eba0
danmcadams <danmcadams@gmail.com>
2025-07-23 17:15:06 -0500

    Refactor Calculator class to add static modifier and implement APY percentage calculation

--- FinancePercentagesCalc/Calculator.cs
+++ FinancePercentagesCalc/Calculator.cs
@@ -1,9 +1,25 @@
 namespace FinancePercentagesCalc;
 
-public class Calculator
+public static class Calculator
 {
     public static double CalculateAPY(int numberOfPeriods, double interestRate)
     {
         return Math.Pow(1 + interestRate / numberOfPeriods, numberOfPeriods) - 1;
+    }
+
+    public static void CalculateAPR()
+    {
+    }
+
+    public static double CalculateAPYPercentage(CompoundFrequency compoundFrequency, int ageOfAccountInMonths, double interestRate)
+    {
+        var apy = compoundFrequency switch
+        {
+            CompoundFrequency.Yearly => CalculateAPY(ageOfAccountInMonths / 12, interestRate),
+            CompoundFrequency.Monthly => CalculateAPY(ageOfAccountInMonths, interestRate),
+            _ => CalculateAPY(365, interestRate)
+        };
+        var apyPercentage = Math.Round(apy * 100, 4);
+        return apyPercentage;
     }
 }
