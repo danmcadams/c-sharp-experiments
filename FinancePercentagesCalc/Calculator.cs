3d4a81203aa110bd5bd138a48417f9efc2b13a25
danmcadams <danmcadams@gmail.com>
2025-07-24 22:31:11 -0500

    Refactor Calculator methods  parameter order and enhance APY/APR calculations

--- FinancePercentagesCalc/Calculator.cs
+++ FinancePercentagesCalc/Calculator.cs
@@ -2,22 +2,23 @@ namespace FinancePercentagesCalc;
 
 public static class Calculator
 {
-    public static double CalculateAPY(int numberOfPeriods, double interestRate)
+    public static double CalculateAPY(double interestRate, int numberOfPeriods)
     {
         return Math.Pow(1 + interestRate / numberOfPeriods, numberOfPeriods) - 1;
     }
 
-    public static void CalculateAPR()
+    public static double CalculateAPR(double apy, int compoundingPeriodsPerYear)
     {
+        return compoundingPeriodsPerYear * (Math.Pow(1 + apy, 1.0 / compoundingPeriodsPerYear)-1);
     }
 
     public static double CalculateAPYPercentage(CompoundFrequency compoundFrequency, int ageOfAccountInMonths, double interestRate)
     {
         var apy = compoundFrequency switch
         {
-            CompoundFrequency.Yearly => CalculateAPY(ageOfAccountInMonths / 12, interestRate),
-            CompoundFrequency.Monthly => CalculateAPY(ageOfAccountInMonths, interestRate),
-            _ => CalculateAPY(365, interestRate)
+            CompoundFrequency.Yearly => CalculateAPY(interestRate,ageOfAccountInMonths / 12),
+            CompoundFrequency.Monthly => CalculateAPY(interestRate, ageOfAccountInMonths),
+            _ => CalculateAPY(interestRate, 365)
         };
         var apyPercentage = Math.Round(apy * 100, 4);
         return apyPercentage;
