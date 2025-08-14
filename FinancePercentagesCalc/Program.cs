46a66441e6635fb092986c059a80292cabb60d44
danmcadams <danmcadams@gmail.com>
2025-08-14 13:46:48 -0500

    Implement 1st menu to select different calculators, might change this.

--- FinancePercentagesCalc/Program.cs
+++ FinancePercentagesCalc/Program.cs
@@ -1,13 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using System.ComponentModel;
 using FinancePercentagesCalc;
 
-var ui = new ConsoleUI();
-ui.Run();
+MenuOption? GetMenuSelection()
+{
+    Console.Clear();
+    Console.WriteLine("Finance Percentages Calculator");
+    Console.WriteLine("------------------------------");
+    foreach (MenuOption option in Enum.GetValues(typeof(MenuOption)))
+    {
+        if (option == MenuOption.Quit)
+            Console.WriteLine("Q. " + GetEnumDescription(option));
+        else
+            Console.WriteLine($"{(int)option}. {GetEnumDescription(option)}");
+    }
+    Console.WriteLine();
+    Console.Write("Select an option: ");
+
+    var key = Console.ReadKey(true).Key;
+    switch (key)
+    {
+        case ConsoleKey.D1:
+        case ConsoleKey.NumPad1:
+            return MenuOption.GeneralCalculator;
+        case ConsoleKey.D2:
+        case ConsoleKey.NumPad2:
+            return MenuOption.SavingsCalculator;
+        case ConsoleKey.Q:
+            return MenuOption.Quit;
+        default:
+            return null;
+    }
+}
+
+bool running = true;
+while (running)
+{
+    var selection = GetMenuSelection();
+    switch (selection)
+    {
+        case MenuOption.SavingsCalculator:
+            var calcUI = new CalcUI();
+            calcUI.Run();
+            break;
+        case MenuOption.Quit:
+            running = false;
+            break;
+        default:
+            Console.WriteLine("Invalid selection. Press any key to try again...");
+            Console.ReadKey();
+            break;
+    }
+}
 
 Console.WriteLine();
 Console.WriteLine("Goodbye.");
 Console.WriteLine();
 
+string GetEnumDescription(MenuOption option)
+{
+    var fi = option.GetType().GetField(option.ToString());
+    var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+    return attributes.Length > 0 ? attributes[0].Description : option.ToString();
+}
 
-
+enum MenuOption
+{
+    [Description("General Calculator")]
+    GeneralCalculator = 1,
+    [Description("Savings Calculator")]
+    SavingsCalculator = 2,
+    [Description("Quit")]
+    Quit = 3
+}
